ae6b67c33b4825b1460e471479d124351580a2b4
Daniele Pietrobelli <daniele.pietrobelli@candide.eu>
2018-02-28 15:12:02 +0000

    adds supports for multiple documents
    - Calvium port

--- ios/RNSync/RNSync.m
+++ ios/RNSync/RNSync.m
@@ -9,20 +9,20 @@
 #import "RNSync.h"
 #import <React/RCTBridge.h>
 #import <React/RCTEventDispatcher.h>
-#import "ReplicationManager.h";
+#import "ReplicationManager.h"
 #import "CloudantSync.h"
 
 
 @implementation RNSync
 {
-    CDTDatastore *datastore;
+    NSMutableDictionary *datastores;
     CDTDatastoreManager *manager;
     CDTReplicator *replicator;
     CDTReplicatorFactory *replicatorFactory;
     NSURL *remoteDatabaseURL;
     RCTResponseSenderBlock replicatorDidCompleteCallback;
     RCTResponseSenderBlock replicatorDidErrorCallback;
-    ReplicationManager* replicationManager;
+    NSMutableDictionary *replicationManagers;
 }
 
 
@@ -40,7 +40,7 @@ RCT_EXPORT_MODULE();
 }
 
 // TODO need to let them name their own datastore! else could conflict with other apps?
-RCT_EXPORT_METHOD(init: (NSString *)databaseUrl callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(init: (NSString *)databaseUrl databaseName:(NSString*) databaseName callback:(RCTResponseSenderBlock)callback)
 {
     // Create a CDTDatastoreManager using application internal storage path
     NSError *error = nil;
@@ -51,16 +51,24 @@ RCT_EXPORT_METHOD(init: (NSString *)databaseUrl callback:(RCTResponseSenderBlock
     NSURL *storeURL = [documentsDir URLByAppendingPathComponent:@"datastores"];
     NSString *path = [storeURL path];
     
-    manager = [[CDTDatastoreManager alloc] initWithDirectory:path error:&error];
+    
+    if(!manager) {
+        manager = [[CDTDatastoreManager alloc] initWithDirectory:path error:&error];
+    }
+    
     
     if(error)
     {
         callback(@[[NSNumber numberWithLong:error.code]]);
         return;
     }
+    if (!datastores) {
+        datastores = [NSMutableDictionary new];
+    }
     
-    // TODO datastore name needs to be configurable
-    datastore = [manager datastoreNamed:@"rnsync_datastore" error:&error];
+    // Make name of the datastore the same than the database
+    // Store this in a dictionary so we can have more than one?
+    datastores[databaseName] = [manager datastoreNamed:databaseName error:&error];
     
     if(error)
     {
@@ -72,22 +80,40 @@ RCT_EXPORT_METHOD(init: (NSString *)databaseUrl callback:(RCTResponseSenderBlock
     
     remoteDatabaseURL = [NSURL URLWithString:databaseUrl];
     
-    replicationManager = [[ReplicationManager alloc] initWithData:remoteDatabaseURL datastore:datastore replicatorFactory:replicatorFactory];
+    if (!replicationManagers) {
+        replicationManagers = [NSMutableDictionary new];
+    }
+    ReplicationManager* replicationManager = [[ReplicationManager alloc] initWithData:remoteDatabaseURL datastore:datastores[databaseName] replicatorFactory:replicatorFactory];
+    
+    replicationManagers[databaseName] = replicationManager;
     
     callback(@[[NSNull null]]);
 }
 
-RCT_EXPORT_METHOD(replicatePush: (RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(replicatePush:(NSString*) databaseName callback:(RCTResponseSenderBlock)callback)
 {
+    ReplicationManager* replicationManager = replicationManagers[databaseName];
+    if (!replicationManager) {
+        NSLog(@"No replication manager found for %@", databaseName);
+        callback(@[[NSNumber numberWithLong:99]]); // TODO: where are these codes defined?
+        return;
+    }
     [replicationManager push: callback];
 }
 
-RCT_EXPORT_METHOD(replicatePull: (RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(replicatePull:(NSString*) databaseName callback:(RCTResponseSenderBlock)callback)
 {
+    ReplicationManager* replicationManager = replicationManagers[databaseName];
+    if (!replicationManager) {
+        NSLog(@"No replication manager found for %@", databaseName);
+        callback(@[[NSNumber numberWithLong:99]]); // TODO: where are these codes defined?
+        return;
+    }
     [replicationManager pull: callback];
 }
 
-RCT_EXPORT_METHOD(create: body id:(NSString*)id callback:(RCTResponseSenderBlock)callback)
+//RCT_EXPORT_METHOD(create: body id:(NSString*)id databaseName:(NSString*) databaseName callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(create: (NSString*) databaseName body: body id:(NSString*)id  callback:(RCTResponseSenderBlock)callback)
 {
     NSError *error = nil;
     
@@ -102,7 +128,6 @@ RCT_EXPORT_METHOD(create: body id:(NSString*)id callback:(RCTResponseSenderBlock
         rev = [CDTDocumentRevision revision];
     }
     
-    
     if(!body)
     {
         body = @{};
@@ -112,7 +137,7 @@ RCT_EXPORT_METHOD(create: body id:(NSString*)id callback:(RCTResponseSenderBlock
     
     // Save the document to the database
     // revision is nil on failure
-    CDTDocumentRevision *revision = [datastore createDocumentFromRevision:rev error:&error];
+    CDTDocumentRevision *revision = [datastores[databaseName] createDocumentFromRevision:rev error:&error];
     if(!revision)
     {
         callback(@[@"document failed to save"]);
@@ -130,11 +155,16 @@ RCT_EXPORT_METHOD(create: body id:(NSString*)id callback:(RCTResponseSenderBlock
     
 }
 
-RCT_EXPORT_METHOD(addAttachment: id name:(NSString*)name path:(NSString*)path type:(NSString*)type callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(addAttachment: (NSString*) databaseName name: id name:(NSString*)name path:(NSString*)path type:(NSString*)type callback:(RCTResponseSenderBlock)callback)
 {
     NSError *error = nil;
     
-    CDTDocumentRevision *revision = [datastore getDocumentWithId:id error:&error];
+    if(!id || !databaseName || !datastores[databaseName]) {
+        callback(@[[NSString stringWithFormat:@"Parameter error, id: %@, database: %@", id, databaseName]]);
+        return;
+    }
+    
+    CDTDocumentRevision *revision = [datastores[databaseName] getDocumentWithId:id error:&error];
     
     if(error)
     {
@@ -150,7 +180,7 @@ RCT_EXPORT_METHOD(addAttachment: id name:(NSString*)name path:(NSString*)path ty
     
     revision.attachments[att.name] = att;
     
-    CDTDocumentRevision *updated = [datastore updateDocumentFromRevision:revision error:&error];
+    CDTDocumentRevision *updated = [datastores[databaseName] updateDocumentFromRevision:revision error:&error];
     
     NSDictionary *dict = @{ @"id" : updated.docId, @"rev" : updated.revId, @"body" : updated.body };
     
@@ -164,12 +194,17 @@ RCT_EXPORT_METHOD(addAttachment: id name:(NSString*)name path:(NSString*)path ty
     }
 }
 
-RCT_EXPORT_METHOD(retrieve: (NSString *)id callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(retrieve: (NSString*) databaseName id: (NSString *)id  callback:(RCTResponseSenderBlock)callback)
 {
     NSError *error = nil;
     
+    if(!id || !databaseName || !datastores[databaseName]) {
+        callback(@[[NSString stringWithFormat:@"Parameter error, id: %@, database: %@", id, databaseName]]);
+        return;
+    }
+    
     // Read a document
-    CDTDocumentRevision *revision = [datastore getDocumentWithId:id error:&error];
+    CDTDocumentRevision *revision = [datastores[databaseName] getDocumentWithId:id error:&error];
     
     if(!error)
     {
@@ -183,17 +218,52 @@ RCT_EXPORT_METHOD(retrieve: (NSString *)id callback:(RCTResponseSenderBlock)call
     }
 }
 
-RCT_EXPORT_METHOD(update: (NSString *)id rev:(NSString *)rev  body:(NSDictionary *)body callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(retrieveAllAttachmentsFor: (NSString*) databaseName id: (NSString *)id callback:(RCTResponseSenderBlock)callback)
 {
     NSError *error = nil;
     
+    // Check if parameters are correct
+    if(!id || !databaseName || !datastores[databaseName]) {
+        callback(@[[NSString stringWithFormat:@"Parameter error, id: %@, database: %@", id, databaseName]]);
+        return;
+    }
+    
     // Read a document
-    CDTDocumentRevision *retrieved = [datastore getDocumentWithId:id rev:rev error:&error];
+    CDTDocumentRevision *revision = [datastores[databaseName] getDocumentWithId:id error:&error];
+    
+    if(!error)
+    {
+        NSDictionary *attachments = revision.attachments;
+        __block  NSMutableDictionary* dataBlobs = [@{} mutableCopy];
+        [attachments enumerateKeysAndObjectsUsingBlock:^(NSString* key, NSObject * object, BOOL *stop){
+            CDTAttachment *att = (CDTAttachment*)object;
+            NSData *imageData = [att dataFromAttachmentContent];
+            NSString* encodedString = [imageData base64EncodedStringWithOptions:0];
+            dataBlobs[att.name] = [NSString stringWithFormat:@"data:%@;base64,%@", att.type, encodedString];
+        }];
+        callback(@[[NSNull null], dataBlobs]);
+    }
+    else{
+        callback(@[[NSNumber numberWithLong:error.code]]);
+    }
+}
+
+RCT_EXPORT_METHOD(update: (NSString*) databaseName id: (NSString *)id rev:(NSString *)rev body:(NSDictionary *)body callback:(RCTResponseSenderBlock)callback)
+{
+    NSError *error = nil;
+    
+    if(!id || !databaseName || !datastores[databaseName]) {
+        callback(@[[NSString stringWithFormat:@"Parameter error, id: %@, database: %@", id, databaseName]]);
+        return;
+    }
+    
+    // Read a document
+    CDTDocumentRevision *retrieved = [datastores[databaseName] getDocumentWithId:id rev:rev error:&error];
     
     retrieved.body = (NSMutableDictionary*)body;
     
-    CDTDocumentRevision *updated = [datastore updateDocumentFromRevision:retrieved
-                                                                   error:&error];
+    CDTDocumentRevision *updated = [datastores[databaseName] updateDocumentFromRevision:retrieved
+                                                                                  error:&error];
     
     NSDictionary *dict = @{ @"id" : updated.docId, @"rev" : updated.revId, @"body" : updated.body };
     
@@ -208,21 +278,21 @@ RCT_EXPORT_METHOD(update: (NSString *)id rev:(NSString *)rev  body:(NSDictionary
 }
 
 
-RCT_EXPORT_METHOD(delete: (NSString *)id callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(delete:(NSString*) databaseName id: (NSString *)id callback:(RCTResponseSenderBlock)callback)
 {
-    if(!id)
-    {
-        //NSArray *params = @[[NSNumber numberWithBool:deleted]];
-        callback(@[@"called delete without specifying the id"]);
-        return;
-    }
     
     NSError *error = nil;
     
-    CDTDocumentRevision *retrieved = [datastore getDocumentWithId:id error:&error];
     
-    [datastore deleteDocumentFromRevision:retrieved
-                                    error:&error];
+    if(!id || !databaseName || !datastores[databaseName]) {
+        callback(@[[NSString stringWithFormat:@"Parameter error, id: %@, database: %@", id, databaseName]]);
+        return;
+    }
+    
+    CDTDocumentRevision *retrieved = [datastores[databaseName] getDocumentWithId:id error:&error];
+    
+    [datastores[databaseName] deleteDocumentFromRevision:retrieved
+                                                   error:&error];
     if(!error)
     {
         //NSArray *params = @[[NSNumber numberWithBool:deleted]];
@@ -233,11 +303,17 @@ RCT_EXPORT_METHOD(delete: (NSString *)id callback:(RCTResponseSenderBlock)callba
     }
 }
 
-RCT_EXPORT_METHOD(deleteDatastore: (RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(deleteDatastoreWithName:(NSString*) databaseName callback:(RCTResponseSenderBlock)callback)
 {
     NSError *error = nil;
     
-    BOOL deleted = [manager deleteDatastoreNamed:@"rnsync_datastore" error: &error];
+    
+    if(!databaseName || !datastores[databaseName]) {
+        callback(@[[NSString stringWithFormat:@"Parameter error, database: %@", databaseName]]);
+        return;
+    }
+    
+    BOOL deleted = [manager deleteDatastoreNamed:databaseName error: &error];
     
     if(!error)
     {
@@ -252,16 +328,22 @@ RCT_EXPORT_METHOD(deleteDatastore: (RCTResponseSenderBlock)callback)
 
 // TODO the results of the query could be huge (run out of memory huge).  Need param for how many items
 // to return and paging to get the rest
-RCT_EXPORT_METHOD(find: (NSDictionary *)query fields:(NSArray *)fields callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(find:(NSString*) databaseName query: (NSDictionary *)query fields:(NSArray *)fields callback:(RCTResponseSenderBlock)callback)
 {
+    
+    if(!databaseName || !datastores[databaseName]) {
+        callback(@[[NSString stringWithFormat:@"Parameter error, database: %@", databaseName]]);
+        return;
+    }
+    
     // TODO waste to new up resultList for every call
     NSMutableArray* resultList = [[NSMutableArray alloc] init];
     
-    CDTQResultSet *result = [datastore find:query
-                                       skip:0
-                                      limit:0
-                                     fields:fields
-                                       sort:nil];
+    CDTQResultSet *result = [datastores[databaseName] find:query
+                                                      skip:0
+                                                     limit:0
+                                                    fields:fields
+                                                      sort:nil];
     
     [result enumerateObjectsUsingBlock:^(CDTDocumentRevision *rev, NSUInteger idx, BOOL *stop)
      {
@@ -274,4 +356,95 @@ RCT_EXPORT_METHOD(find: (NSDictionary *)query fields:(NSArray *)fields callback:
     callback(@[[NSNull null], resultList]);
 }
 
+
+// indexes is a dictionary of dictionaries <String>:<Array<String>>
+// indexes has this shape:
+// @{
+//   @"JSON": @{@"indexName":@[@"property1", @"property2"]},
+//   @"TEXT": @{@"indexName":@[@"property1", @"property2"]}
+// }
+
+// currently types can be:
+// CDTQIndexTypeText = @"TEXT"
+// CDTQIndexTypeJSON = @"JSON"
+
+
+RCT_EXPORT_METHOD(createIndexes:(NSString*) databaseName indexes: (NSDictionary*)indexes callback:(RCTResponseSenderBlock)callback)
+{
+    
+    if(!databaseName || !datastores[databaseName]) {
+        callback(@[[NSString stringWithFormat:@"Parameter error, database: %@", databaseName]]);
+        return;
+    }
+    
+    CDTDatastore* datastore = datastores[databaseName];
+    
+    
+    // TODO: list existing indexes and remove the indexes that need recreation
+    // NSLog(@"indexes %@", [datastore listIndexes]);
+    // [datastore deleteIndexNamed:@"Some name"];
+    
+    
+    // 1. Iterate through the list of types and separate the indexes by that
+    // 2. Take the list of JSON indexes and iterate with something like the thing below
+    // 3. Take the list of TEXT indexes if > 0 check that text search is available
+    // 4. Then iterate the list of TEXT indexes as below
+    // 5. If some index is nil return an error
+    // 6. I all indexes exists then return success and update all indexes
+    
+    NSDictionary* jsonIndexes = indexes[@"JSON"];
+    NSDictionary* textIndexes = indexes[@"TEXT"];
+    
+    __block NSError* error = nil;
+    
+    if(jsonIndexes.count > 0) {
+        
+        [jsonIndexes enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull jsonKey, id  _Nonnull jsonObject, BOOL * _Nonnull stop) {
+            
+            NSString* index = [datastore ensureIndexed:jsonObject
+                                              withName:jsonKey
+                                                ofType:CDTQIndexTypeJSON];
+            if (index == nil) {
+                error = [[NSError alloc] initWithDomain:@"RNSync" code:-2 userInfo:@{@"message": [NSString stringWithFormat:@"Non valid index build for %@ %@", jsonKey, jsonObject]}];
+                *stop = TRUE;
+            }
+        }];
+    }
+    
+    
+    if (textIndexes.count > 0 && !error) {
+        if ([datastore isTextSearchEnabled]) {
+            // We can just set the tokenizer on settings as of now
+            // "porter" is a little bit more fancy than "simple"
+            // http://tartarus.org/~martin/PorterStemmer/
+            // https://www.sqlite.org/fts3.html#tokenizer  (point 8)
+            NSDictionary *settings = @{@"tokenize": @"porter unicode61"};
+            
+            [textIndexes enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull textKey, id  _Nonnull textObject, BOOL * _Nonnull stop) {
+                NSString* index = [datastore ensureIndexed:textObject
+                                                  withName:textKey
+                                                    ofType:CDTQIndexTypeText
+                                                  settings:settings];
+                if (index == nil) {
+                    error = [[NSError alloc] initWithDomain:@"RNSync" code:-2 userInfo:@{@"message": [NSString stringWithFormat:@"Non valid index build for %@ %@", textKey, textObject]}];
+                    *stop = TRUE;
+                }
+            }];
+        } else {
+            NSLog(@"text search is not enabled");
+            error = [[NSError alloc] initWithDomain:@"RNSync" code:-2 userInfo:@{@"message": @"Not possible to create text indexes, review your config"}];
+        }
+    }
+    
+    if (!error) {
+        [datastore updateAllIndexes]; // optimise times after pull but not necessary
+        NSLog(@"indexes %@", [datastore listIndexes]);
+        callback(@[[NSNull null], [datastore listIndexes]]);
+    } else {
+        callback(@[error, [NSNull null]]);
+    }
+    
+}
+
 @end
+
