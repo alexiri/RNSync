9d627f33d56092c8e86ce048e4d53b54a7e73082
Alex Iribarren <alex.iribarren@gmail.com>
2019-07-19 13:35:03 +0200

    Clean up useless code

--- ios/RNSync/RNSync.m
+++ ios/RNSync/RNSync.m
@@ -101,15 +101,6 @@ RCT_EXPORT_METHOD(init: (NSString *)databaseUrl databaseName:(NSString*) databas
         }
     }
 
-
-    if(!manager) {
-        manager = [[CDTDatastoreManager alloc] initWithDirectory:path error:&error];
-    }
-
-    if(error)
-    {
-        datastores = [NSMutableDictionary new];
-    }
     if (!datastores) {
         datastores = [NSMutableDictionary new];
     }
